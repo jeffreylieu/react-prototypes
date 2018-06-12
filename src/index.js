13a9e993aba9a61ec00d6a6d3547d3211e1b7736
Jeff Lieu <jefflieu@gmail.com>
2018-06-12 09:49:43 -0700

    submitting react-intro prototype

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+function luckyNumber(){
+    const element = Math.floor((Math.random() * 1000) + 1);
+    return element;
+}
+
+
+var greeting = function(userObj){
+    const element = (
+        <div className='container'>
+            <h1>Hello!{userObj.name}</h1>
+            <h2 className='text-muted'>The lucky number is :{user.luckyNumber}</h2>
+        </div>
+    )
+    return element;
+}
+// check from here
+const element2 = {name: 'moocow'};
+
+const user = {
+    name: 'boo boo',
+    luckyNumber: luckyNumber(Math.random()),
+}
+
+
+
+// this suppose to stay at the bottom of the page
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
