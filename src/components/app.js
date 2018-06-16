1080b8be481ea3d4175a1db516441988c6561291
Jeff Lieu <jefflieu@gmail.com>
2018-06-16 12:15:20 -0700

    commiting functional_components prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,29 @@
+import React from 'react';
+import Table from './table';
 
+export default () => {
+    return (
+        <div className="container">
+            <h1>"Student Grade Table"</h1>
+            <Table data={students}/>
+        </div>
+    )
+}
 
+const students = [
+    {
+        student: 'Leonard Chan',
+        course: 'sex ed',
+        grade: 90
+    },
+    {
+        student: 'Josh Wilcox',
+        course: 'sex ed',
+        grade: 100
+    },
+    {
+        student: 'Chris Kosen',
+        course: 'sex ed',
+        grade: 50
+    },
+];
