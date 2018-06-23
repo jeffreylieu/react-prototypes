3825cffa35b53f4288af5846c0f8b8e4a1c2e9e1
Jeff Lieu <jefflieu@gmail.com>
2018-06-22 17:21:19 -0700

    finished react-axios

--- src/components/movie_container.js
+++ src/components/movie_container.js
@@ -15,10 +15,9 @@ class MoviesContainer extends Component {
     componentWillMount(){
         const url = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json';
         axios.get(url).then((resp)=>{
-            console.log('RESP:', resp);
 
             this.setState({
-                movies: resp.data.feed.entry //left off here!!
+                movies: resp.data.feed.entry
             });
         });
     }
@@ -27,10 +26,9 @@ class MoviesContainer extends Component {
         const movieList = this.state.movies.map((movieInfo, index)=>{
             return <Movie info={movieInfo} key={index}/>
         });
-        console.log('This state is:', this.state);
         return(
             <div>
-                <Movie/>
+                {movieList}
             </div>
         )
     }
--- src/components/movies.js
+++ src/components/movies.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 
 export default props => {
+    const{info}= props;
     return(
         <div>
-            <h3>Movie</h3>
+            <img src={info['im:image'][2].label}/>
+            <h3>{info['im:name'].label}</h3>
+            <p>{info.summary.label}</p>
         </div>
     );
 }
